11552bd07dd5840c431bab49ef9ce4e124aea2d6
matteo-293473 <m.pulcinelli@campus.uniurb.it>
2021-11-15 12:24:58 +0100

    sistemate cartelle

---
+++ sql/qrApp.sql
@@ -0,0 +1,43 @@
+-- creazione database
+-- CREATE DATABASE qrApp;
+
+-- creazione delle tabelle
+
+
+------------------------------------------------- 
+
+/* CREATE TABLE persona(
+    id_persona serial primary key
+    
+);
+
+CREATE TABLE dispostivo(
+    id_device varchar(30) primary key
+    
+);
+
+CREATE TABLE lettura(
+    id_persona int,
+    orario_entrata timestamp default now(),
+    orario_uscita timestamp default now(),
+    quantita int,
+    foreign key (id_persona) references persona(id_persona),
+    primary key (id_persona,orario_entrata)
+); */
+
+------------------------------------------------- 
+
+CREATE TABLE dispostivo(
+    id_device varchar(30) primary key
+    
+);
+
+CREATE TABLE lettura(
+    id_device varchar(30),
+    orario_entrata timestamp default now(),
+    orario_uscita timestamp default now(),
+    quantita int,
+    qrInfo varchar(200),
+    foreign key (id_device) references dispostivo(id_device),
+    primary key (id_device,orario_entrata)
+);
